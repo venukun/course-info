8287bada05b8299d8ed9328bec8a41299738c0bd
Venu Kunduru <kunduruvenu@gmail.com>
2022-11-23 01:10:10 +0530

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import CourseListing from "./containers/CourseListing";
 import Header from "./containers/Header";
 import "./App.css";
@@ -8,14 +8,14 @@ import CourseDetails from "./containers/CourseDetails";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Header />
         <Switch>
           <Route path="/" exact component={CourseListing} />
           <Route path="/course/:courseId" component={CourseDetails} />
           <Route>404 Not Found!</Route>
         </Switch>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
